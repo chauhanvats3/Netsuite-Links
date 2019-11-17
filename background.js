22526de1cf0736237f9f1dd196a154b983c25f5d
chauhanvats3 <chauhan.vats3@gmail.com>
2019-11-18 03:59:49 +0530

    Added Module Loader
    Added a functionality to load client-side modules to current page

---
+++ background.js
@@ -0,0 +1,39 @@
+chrome.extension.onConnect.addListener(function (port) { //Listen to any incoming messages
+    port.onMessage.addListener(function (msg) {
+        var msgRcvd = msg.codeToInject;
+        var splittedArr = msgRcvd.split(",");
+        var modVarName = splittedArr[0];
+        var modName = splittedArr[1];
+        //Define a string to tell chrome api to inject a piece of code
+        var codeBGtoPage = "(function() {";
+        //Overriding window.onError
+        codeBGtoPage += "var codeToExecute ='window.onerror = function (msg, url, line, col, error) {if (error.TYPE == \"error.SuiteScriptModuleLoaderError\") { alert(error.message);} else { var extra = !col ? \"\" : \"\\\ncolumn: \" + col; extra += !error ? \"\" : \"\\\nerror: \" + error;console.log(\"Error: \" + msg + \"\\\nurl: \" + url + \"\\\n line: \" + line + extra);} var suppressErrorAlert = true;return suppressErrorAlert;};'";
+        //using require()
+        codeBGtoPage += ";codeToExecute+= ';require([\"" + modName + "\"],function(){window[\"" + modVarName + "\"] = require(\"" + modName + "\"); alert(\"Module : " + modName + "\\\n VariableName : " + modVarName + "\\\n Enjoy!\");  });';";
+       //creating a <script> tag to insert script
+        codeBGtoPage += ";var script = document.createElement('script');"; //So ,we had to do all this charade because sending require directly wouldn't work
+        codeBGtoPage += "script.textContent = codeToExecute;";
+        codeBGtoPage += "document.body.appendChild(script);";
+        codeBGtoPage += "})();";
+
+        //sending this code tp page for injection
+        chrome.tabs.executeScript({
+            code: codeBGtoPage
+        });
+        port.postMessage({
+            joke: "Hi Popup.js"
+        });
+
+    });
+});
+
+/*  Nothing special going on here, we tell chrome.tabs.executeScript to execute the code given inside string to be executed in the page.
+    that function inside the string creates a <script> tag and append it to the <body> tag
+    as soon as the script tag gets inserted, it starts executing in the web page's context.
+    We couldn't do it the normal way because I guess require() exists only for the web page and not for the chrome
+*/
+/*Structure : 
+            1. " aka doubleQuotes to enclose string to be inserted in codeBGtoPage
+            2. ' aka singleQuote to enclose strings that are to be inserted in codeToExecute
+            3. \"  aka escape doubleQuote to enclose strings as strings and NOT code in codeToExecute
+             */
--- popup.js
+++ popup.js
@@ -1,86 +1,158 @@
 document.addEventListener('DOMContentLoaded', function () {
     var finalLink = {};
-
+    var formSearch = document.getElementById("searchForm");
+    var btnSearch = document.getElementById("btnSearch");
+    var inputModuleName = document.getElementById("moduleNameInput");
+    var formLoadModule = document.getElementById("loadModuleForm");
+    var btnLoadModule = document.getElementById("btnLoadModule");
+    var btnGithub = document.getElementById("btnGithub");
+    var inputSearch = document.getElementById("inputSearch");
+    //funtion to submit SuiteAnswers Query
     function functSubmit(event) {
-        var searchQuery = document.getElementById("input1").value;
+        var searchQuery = document.getElementById("input_search").value;
         var ansLink = "";
         if (searchQuery.trim() != "")
             ansLink = "https://netsuite.custhelp.com/app/answers/list/st/5/kw/" + searchQuery;
         else
-            ansLink = "https://netsuite.custhelp.com/"
+            ansLink = "https://netsuite.custhelp.com/";
         chrome.tabs.create({
             active: true,
             url: ansLink
         });
     }
-    document.getElementById("searchForm").addEventListener('submit', functSubmit);
-    document.getElementById("btnSearch").addEventListener('click', functSubmit);
+
+    //Assign both enter event and click event
+    formSearch.addEventListener('submit', functSubmit);
+    btnSearch.addEventListener('click', functSubmit);
 
     var queryInfo = {
         'url': "https://*.app.netsuite.com/*"
     };
     var defaultAccName = 'tstdrv2152924';
 
-    //TODO: add button up listener on input field
-
     chrome.tabs.query(queryInfo, function (tabs) {
-        console.log("Tabs : " + tabs);
-        var tabsPresent = false;
-        if (tabs == '' || tabs == void 0) {
+        if (tabs == '' || tabs == void 0) { //if no netsuite tab is open
             var halfButtons = document.getElementsByClassName("btn btn-primary hasHalfValue");
             for (var j = 0; j < halfButtons.length; j++) {
                 (function () {
                     var btn = halfButtons[j];
-                    btn.style.display = "none";
+                    btn.style.display = "none"; //hide buttons with half url
                 })();
             }
-            console.log("Tabs not Open");
+            //   console.log("Tabs not Open");
         }
         var buttons = document.getElementsByTagName("button");
         for (var i = 0; i < buttons.length; i++) {
 
             (function () {
                 var btn = buttons[i];
-                if (btn.style.display != "none") {
-                    var classNameBtn = btn.className;
-                    var isClassHalfValue = classNameBtn.includes("hasHalfValue");
-                    var btnId = btn.id;
-                    console.log("Class = " + classNameBtn);
-                    if (isClassHalfValue) {
-                        var url = tabs[0].url;
-                        var urlWithoutScheme = url.slice(8);
-                        var splittedUrlArr = urlWithoutScheme.split(".", 1);
-                        var extractedAccName = splittedUrlArr[0];
-                        var scheme = "https://";
-                        var accName = extractedAccName ? extractedAccName : defaultAccName;
-                        var location = btn.value;
-                        // finalLink = "" + scheme + accName + location;
-                        finalLink[btnId] = "" + scheme + accName + location;
-                        console.log("Final Link Half Value : " + finalLink);
-                    } else {
-                        // finalLink = btn.value;
-                        finalLink[btnId] = btn.value;
-                        console.log("Final Link Full Value : " + finalLink);
+                if (btn.id != 'btnGithub') {
+                    if (btn.style.display != "none") { //for the buttons that are visible
+                        var classNameBtn = btn.className;
+                        var isClassHalfValue = classNameBtn.includes("hasHalfValue");
+                        var btnId = btn.id;
+                        // console.log("Class = " + classNameBtn);
+                        if (isClassHalfValue) { //for those who have half link, get the account id
+                            var url = tabs[0].url;
+                            var urlWithoutScheme = url.slice(8);
+                            var splittedUrlArr = urlWithoutScheme.split(".", 1);
+                            var extractedAccName = splittedUrlArr[0];
+                            var scheme = "https://";
+                            var accName = extractedAccName ? extractedAccName : defaultAccName;
+                            var location = btn.value;
+                            finalLink[btnId] = "" + scheme + accName + location;
+                        } else { //for those with full link
+                            finalLink[btnId] = btn.value;
+                        }
+                        btn.onclick = function () { //assign on click to each button accordingly
+                            var urlLink = finalLink[this.id];
+                            chrome.tabs.create({
+                                active: true,
+                                url: urlLink
+                            });
+                        };
                     }
-                    btn.onclick = function () {
-                        var urlLink = finalLink[this.id];
-                        chrome.tabs.create({
-                            active: true,
-                            url: urlLink
-                        });
-                    };
-                    console.log("Link added : " + finalLink + " On button : " + btn.id);
-
                 }
             })();
         }
-        var myButtons = document.getElementsByTagName("button");
-        for (var k = 0; k < myButtons.length; k++) {
-            var btn = myButtons[k];
-            console.log("BTN id : " + btn.id + " onClick = " + btn.onclick);
-        }
-
     });
 
+    function loadModule() {
+        inputModuleName.value.trim();
+        if (inputModuleName != "" || inputModuleName != void 0) {
+            if (inputModuleName.startsWith("N/") || inputModuleName.startsWith("SuiteScripts/")) {
+                console.log("Module Name : " + inputModuleName);
+                var tempModuleName = "";
+                if (inputModuleName.endsWith(".js")) {
+                    tempModuleName = inputModuleName.substring(0, inputModuleName.length - 3);
+                } else {
+                    tempModuleName = inputModuleName;
+                }
+
+                var splittedModuleNameArr = tempModuleName.split("/");
+                var moduleVariableName = splittedModuleNameArr[splittedModuleNameArr.length - 1];
+                dataToSend = "" + moduleVariableName + "," + inputModuleName;
+
+                var port = chrome.extension.connect({ //Create  a port to connect with background.js
+                    name: "Module Communication"
+                });
+                port.postMessage({ //message background script to do something
+                    codeToInject: dataToSend
+                });
+                port.onMessage.addListener(function (msg) { //Get a feedBack from Background script
+                    console.log("message recieved from bg" + msg.joke);
+                });
+            } else {
+                alert("Supported modules are 'N/*' or 'SuiteScripts/*' only");
+            }
+        } else {
+            alert("Module Name Invalid");
+        }
+    }
+
+    formLoadModule.addEventListener('submit', loadModule);
+    btnLoadModule.addEventListener('click', loadModule);
+    btnGithub.addEventListener('click', goToGithub);
+
+    function goToGithub() {
+        var urlGithub = 'https://github.com/chauhanvats3/Netsuite-Links';
+        chrome.tabs.create({
+            active: true,
+            url: urlGithub
+        });
+    }
+
+    inputSearch.addEventListener('input', inputChanged);
+    inputModuleName.addEventListener('input', inputChanged);
+
+    function inputChanged(e) {
+        var caller = e.target || e.srcElement;
+
+        if (caller.value) {
+            console.log("Caller Has Value");
+            caller.className = "inputHasContent";
+
+            switch (caller.id) {
+
+                case 'inputSearch':
+                    formSearch.className = "formHasContent";
+                    break;
+                case 'moduleNameInput':
+                    formLoadModule.className = "formHasContent";
+                    break;
+            }
+        } else {
+            caller.className = caller.className.replace(/\binputHasContent\b/g, "");
+            console.log("Caller Doesn't have Value");
+            switch (caller.id) {
+                case 'inputSearch':
+                    formSearch.className = formSearch.className.replace(/\bformHasContent\b/g, "");
+                    break;
+                case 'moduleNameInput':
+                    formLoadModule.className = formLoadModule.className.replace(/\bformHasContent\b/g, "");
+                    break;
+            }
+        }
+    }
 
 });
