3c908c6346267aee6a7fc6b63464f0603dbb1842
Vatsal Chauhan <chauhan.vats3@gmail.com>
2020-04-16 19:07:21 +0530

    Added Multiple Instance Loading Functionality

--- cntntScrptMultiInstance.js
+++ cntntScrptMultiInstance.js
@@ -4,15 +4,15 @@ $(document).ready(function ($) {
         var instanceId = pageURL.split(".app.")[0].split("://")[1];
 
         chrome.storage.local.get("allInstancesData", function (items) {
-            // console.log("Got from memory : " + JSON.stringify(items));
+            console.log("Got from memory : " + JSON.stringify(items));
             if (jQuery.isEmptyObject(items))
                 saveThisInstanceData(items, instanceId);
             else {
                 if (!items.allInstancesData.hasOwnProperty(instanceId))
                     saveThisInstanceData(items.allInstancesData, instanceId);
                 else
-                    // console.log("This account data already exists");
-                ;
+                    console.log("This account data already exists");
+                //;
             }
         });
     }
@@ -21,15 +21,17 @@ $(document).ready(function ($) {
 function saveThisInstanceData(allInstancesData, instanceId) {
     var companyNinstance = jQuery(".ns-role-company")[0].innerHTML;
     var instUsrName = jQuery(".ns-role > span")[0].innerHTML;
+    var instURL = "https://" + instanceId + ".app.netsuite.com/app/center/card.nl";
     allInstancesData[instanceId] = {};
     allInstancesData[instanceId] = {
         id: instanceId,
         company_and_instance: companyNinstance,
-        instanceUserName: instUsrName
+        instanceUserName: instUsrName,
+        url: instURL
     };
     chrome.storage.local.set({
         "allInstancesData": allInstancesData
     }, function () {
-        // console.log("Saved To Memory : " + JSON.stringify(allInstancesData));
+        console.log("Saved To Memory : " + JSON.stringify(allInstancesData));
     });
 }
--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var whichInputBox = "";
     var moduleLoadDisabled = false;
     var btnRecToJson = document.getElementById("btnRecToJson");
-
+    setupOtherInstanceBtns();
     $(function () {
         var availableModules = [
             "N/record",
@@ -126,12 +126,15 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     chrome.tabs.query(getAllNetsuiteTabsQuery, function (allNetsuiteTabs) {
         console.log("Query for all NS tabs fired");
+
         if (allNetsuiteTabs.length == 0) { //if no netsuite tab is open
             console.log("no netsuite tab open");
             disableModuleInput();
             disableRecToJSON();
 
+
         } else { //netsuite tab is open
+
 
             console.log("Netsuite tab open");
             var getCurrentTabQuery = {
@@ -170,7 +173,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 var classNameBtn = btn.className;
                 var isClassHalfValue = classNameBtn.includes("hasHalfValue");
                 var btnId = btn.id;
-                // console.log("Class = " + classNameBtn);
+                console.log("id: " + btnId + "Class = " + classNameBtn);
                 if (isClassHalfValue) {
                     var scheme, accName, location;
                     scheme = "https://";
@@ -190,9 +193,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 } else { //for those with full link
                     finalLink[btnId] = btn.value;
-                }
 
-                if (btn.id == 'btnRecToJson') {
+                }
+                if (btn.id == "btnMultiInstances") {
+                    btn.addEventListener("click", multiInstClick);
+                } else if (btn.id == 'btnRecToJson') {
                     btnRecToJson.addEventListener('click', convtRecToJson);
                 } else {
 
@@ -340,23 +345,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+
     /*================================Multi Instance Loader=================== */
-    function ldisplayOtherInstanceData() {
-        var instanceId = "5406540-sb2";
+    function setupOtherInstanceBtns() {
         chrome.storage.local.get("allInstancesData", function (items) {
             // console.log("Got from memory : " + JSON.stringify(items));
             if (jQuery.isEmptyObject(items))
                 console.log("Data enmpty");
             else {
-                if (!items.allInstancesData.hasOwnProperty(instanceId))
-                    console.log("Selected instance data not available");
-                else {
-                    console.log("Data : " + JSON.stringify(items));
-                    // console.log("This account data already exists");
-
+                var allInstanceData = items.allInstancesData;
+                for (var eachInstKey in allInstanceData) {
+                    var eachInstData = allInstanceData[eachInstKey];
+                    var compNInst = eachInstData.company_and_instance;
+                    var instanceId = eachInstData.id;
+                    var instUsrName = eachInstData.instanceUserName;
+                    var instURL = eachInstData.url;
+                    var eachBtnStr = `<button type="button" id="btn${instanceId}" value="${instURL}" class="button hasFullValue instanceLink">${compNInst} </br> ${instUsrName}</button>`;
+                    $(eachBtnStr).insertBefore("#multInstLead");
                 }
             }
         });
+        $("#multiInstanceDiv")[0].style.display = "none";
+    }
+
+    function multiInstClick() {
+        $("#outerDiv")[0].style.display = "none";
+        $("#multiInstanceDiv")[0].style.display = "flex";
     }
     /*=====================Multi Instance Loader Ends=============== */
+
 });
